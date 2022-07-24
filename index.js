46d1abf994347e9f69cbd93ddf189761879de3d8
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-24 20:03:56 -0300

    Add views and res.sendFile

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res)=>{
-    res.send("<h1>Home Page</h1>")
+    res.sendFile("./views/index.html", {root: __dirname})
 })
 
 app.get("/about", (req, res)=>{
-    res.send("<h1>About Page</h1>")
+    // res.send("<h1>About Page</h1>")
+    res.sendFile("./views/about.html", {root: __dirname})
 })
 
 app.use((req, res)=>{
-    res.status(404).send("<h1>Not Found</h1>")    
+    res.status(404).sendFile("./views/404.html", {root: __dirname})  
 })
 
 app.listen(3000, ()=>{
