fc45c5ff6efbcc3485297ebfc67b7608199e2992
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-24 20:37:57 -0300

    Add 'ejs' templates with res.render. Also add new route: '/blogs/create'

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express")
 const app = express()
 
+// Set EJS to be the view engine
+app.set("view engine", "ejs")
+
 app.get("/", (req, res)=>{
-    res.sendFile("./views/index.html", {root: __dirname})
+    res.render("index")
 })
 
 app.get("/about", (req, res)=>{
     // res.send("<h1>About Page</h1>")
-    res.sendFile("./views/about.html", {root: __dirname})
+    // res.sendFile("./views/about.html", {root: __dirname})
+    res.render("about")
+})
+
+app.get("/blogs/create", (req, res)=>{
+    res.render("create")
 })
 
 app.use((req, res)=>{
-    res.status(404).sendFile("./views/404.html", {root: __dirname})  
+    res.status(404).render("404")  
 })
 
 app.listen(3000, ()=>{
