2c9ffdad671943e655a02576568307b93c5ef879
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-26 00:52:59 -0300

    Add mongoDb connection and Model. Verify connection ok with some dummy data

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const mongoose = require("mongoose")
+const Blog = require("./models/Blog")
 const express = require("express")
 const app = express()
+
+
+//.env file
+require("dotenv").config()
 
 // Set EJS to be the view engine
 app.set("view engine", "ejs")
@@ -16,22 +22,40 @@ app.use(express.static("public"))
 
 // Routes
 app.get("/", (req, res)=>{
-    console.log(data)
-    res.render("index", {data, title:"Blogs"})
+    Blog.find({}).sort({createdAt: -1})
+    .then((result)=>{
+        res.render("index", {data: result, title:"Blogs"})
+    })
+    // console.log(data)
+    // res.render("index", {data, title:"Blogs"})
 })
 
 app.get("/about", (req, res)=>{
     res.render("about", {title:"About"})
 })
 
-app.get("/blogs/create", (req, res)=>{
-    res.render("create", {title:"Add new blog"})
+app.get("/blogs/create", async (req, res)=>{
+    const newBlog = new Blog(
+        {
+            title: "Something NEW", 
+            shortDescription: "Some quotes from the writer", 
+            content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque suscipit recusandae modi?"
+        }
+    )
+    newBlog.save().then(()=>res.redirect("/")) 
+    // res.render("create", {title:"Add new blog"})
 })
 
 app.use((req, res)=>{
     res.status(404).render("404")  
 })
 
-app.listen(3000, ()=>{
-    console.log("Listening in port 3000")
-})
+mongoose.connect(process.env.MONGO_URI)
+.then(
+    ()=>{
+        app.listen(3000, ()=>{
+            console.log("Connected to MongoDB & Listening in port 3000")
+        })
+    }
+)
+.catch(err => console.log(err))
