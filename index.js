c126c6ec72d22f4067759c77574dbffcea35cd56
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-26 04:24:38 -0300

    Add controllers. Logic completed for all actions except 'update'. Update style and add delete-icon

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
 const Blog = require("./models/Blog")
+const { getAllBlogs, createBlog, showForm, getBlog, deleteBlog} = require("./controllers/blogControllers")
 const express = require("express")
 const app = express()
-
+var methodOverride = require('method-override')
 
 //.env file
 require("dotenv").config()
@@ -10,52 +11,38 @@ require("dotenv").config()
 // Set EJS to be the view engine
 app.set("view engine", "ejs")
 
-
-// Dummy data to pass to the "ejs" template
-const data = [
-    {title: "Borges Quotes", shortDescription: "Some quotes from the writer", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque suscipit recusandae modi?"}, 
-    {title: "Random ideas into text", shortDescription: "Ideas, where do they come from?", content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque suscipit recusandae modi?"} 
-]
-
 //Middleware & static files
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))  // override with POST having ?_method=DELETE
+
 
 // Routes
-app.get("/", (req, res)=>{
-    Blog.find({}).sort({createdAt: -1})
-    .then((result)=>{
-        res.render("index", {data: result, title:"Blogs"})
-    })
-    // console.log(data)
-    // res.render("index", {data, title:"Blogs"})
-})
-
+// ---------------------------------------------------
 app.get("/about", (req, res)=>{
     res.render("about", {title:"About"})
 })
 
-app.get("/blogs/create", async (req, res)=>{
-    const newBlog = new Blog(
-        {
-            title: "Something NEW", 
-            shortDescription: "Some quotes from the writer", 
-            content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque suscipit recusandae modi?"
-        }
-    )
-    newBlog.save().then(()=>res.redirect("/")) 
-    // res.render("create", {title:"Add new blog"})
-})
+app.get("/", getAllBlogs)
+
+app.get("/blogs/create", showForm)
+
+app.post("/blogs", createBlog)
+
+app.get("/blogs/:id", getBlog)
+
+app.delete("/blogs/:id", deleteBlog)
 
 app.use((req, res)=>{
     res.status(404).render("404")  
 })
 
+
+// Connect to MongoDB and Run app
 mongoose.connect(process.env.MONGO_URI)
-.then(
-    ()=>{
+    .then( ()=>{
         app.listen(3000, ()=>{
             console.log("Connected to MongoDB & Listening in port 3000")
         })
-    }
-)
-.catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
