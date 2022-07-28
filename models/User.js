c828c3540b8906fded4f9b9b97d0876078f93862
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-28 14:08:37 -0300

    Hash the password before saving to database

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -13,6 +14,22 @@ const UserSchema = new mongoose.Schema({
         minlength: 6,
     }
 }, {timestamps:true})
+
+
+// Run a function before a doc is saved in the database - Use "function" to access "this"
+// Hash the password before saving to database
+UserSchema.pre("save", async function (next) {
+    // console.log("Run this function BEFORE saved doc to db", this)
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt) 
+    next()
+})
+
+// Run a function after a doc is saved in the database
+// UserSchema.post("save", (doc, next) => {
+//     console.log("Run this function AFTER saved doc to db", doc)
+//     next()
+// })
 
 const User = mongoose.model("user", UserSchema)
 
